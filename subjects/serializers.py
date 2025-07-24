66019e426ab78d9852b2c955e5351d9bd1bb3b1d
Morris <soulvibe.com@gmail.com>
2025-07-24 11:51:00 +0000

    add the admin/subjects and academic year

--- subjects/serializers.py
+++ subjects/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from .models import Subject
-from levels.serializers import LevelSerializer
+from levels.models import Level
 
 class SubjectSerializer(serializers.ModelSerializer):
-    level = LevelSerializer(read_only=True)
-
     class Meta:
         model = Subject
         fields = ['id', 'subject', 'level', 'updated_at']
-        read_only_fields = ['id', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+    def validate_level(self, value):
+        if not Level.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Invalid level ID")
+        return value
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['level_id'] = instance.level.id  # Include level_id for frontend
+        return representation
